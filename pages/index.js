4611768b31f9fa0ad70f122c1e203dd317464c1c
Lee Griffiths <leegriffiths@caci.co.uk>
2023-11-29 20:05:16 +0000

    Changed Welcome Message

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Welsh Grand Tour</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to WGT" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to the  Welsh Grand Tour <code>Winter Edition 23/34</code>
         </p>
       </main>
 
